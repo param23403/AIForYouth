07c7e20099538879cfedb2f23df9f59d0da3f79e
Param <param23403@gmail.com>
2024-10-24 11:00:43 -0400

    template with navigation

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  //   images: {
+  //     remotePatterns: [
+  //       {
+  //         protocol: "https",
+  //       },
+  //     ],
+  //   },
+};
+
+export default nextConfig;
