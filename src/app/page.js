c2d0de46f359cfe5bfeb8bdf58ffe7aa7c51b506
Param <param23403@gmail.com>
2024-11-25 13:30:57 -0500

    updated homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,49 @@
 export default function Home() {
   return (
-    <>
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        Hello world
+    <main className="min-h-screen w-full bg-white">
+      <div className="container mx-auto px-8 py-12 sm:px-12 sm:py-16">
+        <div className="prose-xl max-w-none">
+          <p className="text-lg sm:text-xl font-semibold text-gray-800 leading-relaxed">
+            The AI for Youth ExLent Explorations project focuses on
+            democratizing access to cutting-edge artificial intelligence (AI)
+            education, ensuring inclusivity for students from diverse
+            backgrounds, particularly underrepresented minorities and low-income
+            high school students. The program aims to prepare the next
+            generation of AI innovators by addressing challenges like
+            insufficient teacher training and the absence of a structured AI
+            curriculum in underrepresented schools. It emphasizes equity and
+            fairness in AI education to empower students to pursue AI-related
+            careers and use AI to address social issues.
+          </p>
+          <br />
+          <br />
+          <p className="text-lg sm:text-xl font-semibold text-gray-800 leading-relaxed">
+            The initiative also supports high school teachers by providing
+            training in AI, enabling them to deliver an innovative curriculum
+            designed to inspire students. It combines transformative learning
+            experiences with skill development in communication, leadership,
+            teamwork, and critical thinking. Key features include a paid
+            internship program for students, mentorship pairings between
+            faculty, graduate students, teachers, and students, and team-based,
+            action-oriented activities. Overall, the project seeks to build a
+            diverse, ethical AI workforce equipped to solve real-world
+            challenges while promoting inclusivity and equity in AI education
+            and innovation.
+          </p>
+          <br />
+          <br />
+          <div className="text-lg sm:text-xl font-semibold text-gray-800 leading-relaxed">
+            <a
+              href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=2347281"
+              className="text-blue-600 hover:text-blue-800 hover:underline transition-colors"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Link to National Science Foundation Award
+            </a>
+          </div>
+        </div>
       </div>
-    </>
+    </main>
   );
 }
